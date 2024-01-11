d82771d282ae2d32798f7b35c3da1fdd748e7368
arhan <arhanersan@gmail.com>
2024-01-11 18:19:08 +0300

    revised

--- hmk1/src/SoftHwNumbers/SoftHwNumbers.java
+++ hmk1/src/SoftHwNumbers/SoftHwNumbers.java
@@ -12,7 +12,7 @@ public class SoftHwNumbers {
         System.out.println("Enter the second number:");
         int num2 = scanner.nextInt();
 
-        System.out.println("The odd numbers between " + num1 + " and " + num2 + " are:");
+        System.out.println("The even numbers between " + num1 + " and " + num2 + " are:");
         for (int i = num1 + 1; i < num2; i++) {
             if (i % 2 == 0) {
                 System.out.print(i + ",");
